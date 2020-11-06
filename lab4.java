ca53079e4bd9c00d033221b5b053ef5c7aaedfb9
Mescruu <mescruu@gmail.com>
2020-11-06 11:52:47 +0100

    v3

--- lab4.java
+++ lab4.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;  // Import the Scanner class
+
 public class Lab4 {
    public static void main(String[] args) {
-      System.out.println("Hello, World");
+      Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+      System.out.println("Enter username");
+
+      String userName = myObj.nextLine();  // Read user input
+      System.out.println("Username is: " + userName);  // Output user input
    }
 }
